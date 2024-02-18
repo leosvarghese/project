d05fd34e7331319aacbe350b197b80e236f2a6d1
leosvarghese <leos_v@yahoo.com>
2024-02-18 20:31:45 +0530

    u

--- c1-versions.tf
+++ c1-versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.0"
+      version = ">=3.0"
     }
   }
 }
 # Provider Block
 
 provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
-
 
 
