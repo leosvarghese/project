cec44310e833a01f2c90faa8df50276d94a81039
leosvarghese <leos_v@yahoo.com>
2024-02-19 12:31:52 +0530

    u

--- c1-versions.tf
+++ c1-versions.tf
@@ -11,7 +11,6 @@ terraform {
 # Provider Block
 
 provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
